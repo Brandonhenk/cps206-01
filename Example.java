752e40a4888f5eaa97aa82d2a7258455dbf4180b
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-01-31 08:23:59 -0600

    Build Class

---
+++ Example.java
@@ -0,0 +1,8 @@
+import java.util.Arraylist;
+  public class Example {
+    public static void main(String[] args){
+      Arraylist<String> list = new Arrayist<>
+    }
+  }
+
+//<String extends Compaiable<? Super String>
