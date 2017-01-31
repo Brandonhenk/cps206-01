cd6af4b939b54c20bf8ad9f6a5844c86c416ce11
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-01-31 08:34:24 -0600

    Add Method

--- Example.java
+++ Example.java
@@ -1,7 +1,12 @@
-import java.util.Arraylist;
+import java.util.ArrayList;
   public class Example {
     public static void main(String[] args){
-      Arraylist<String> list = new Arrayist<>
+      ArrayList<String> list = new ArrayList();
+      System.out.println(list.size());
+      list.add ("Brandon");
+      list.add (0,"Henk");
+      System.out.println(list.size());
+      System.out.println(list);
     }
   }
 
